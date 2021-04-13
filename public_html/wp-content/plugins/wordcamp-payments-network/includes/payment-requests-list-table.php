DomDev126/wordcamp.org

commit 2eb4f8347839021c9cae23e733751b84331529b7

WordCamp Payments Network: Strip commas from search to match raw.
Previously, searching for `1,234` would not find an item with an amount of `1234` in the index table.

public_html/wp-content/plugins/wordcamp-payments-network/includes/payment-requests-list-table.php
@@ -77,7 +77,12 @@ class Payment_Requests_List_Table extends WP_List_Table {
 		}
 
 		if ( ! empty( $_REQUEST['s'] ) ) {
-			$where .= $wpdb->prepare( ' AND `keywords` LIKE %s ', '%' . $wpdb->esc_like( wp_unslash( $_REQUEST['s'] ) ) . '%' );
+			$query = Budgets_Dashboard\formatted_amount_to_float( $_REQUEST['s'] );
+
+			$where .= $wpdb->prepare(
+				' AND `keywords` LIKE %s ',
+				'%' . $wpdb->esc_like( $query ). '%'
+			);
 		}
 
 		if ( ! empty( $_REQUEST['orderby'] ) && in_array( $_REQUEST['orderby'], array_values( $this->get_sortable_columns() ) ) ) {

public_html/wp-content/plugins/wordcamp-payments-network/includes/reimbursement-requests-list-table.php
@@ -60,7 +60,9 @@ class Reimbursement_Requests_List_Table extends WP_List_Table {
 		if ( ! empty( $_REQUEST['s'] ) ) {
 			$search = $wpdb->prepare(
 				"AND `keywords` LIKE '%%%s%%'",
-				$wpdb->esc_like( wp_unslash( $_REQUEST['s'] ) )
+				$wpdb->esc_like(
+					Budgets_Dashboard\formatted_amount_to_float( $_REQUEST['s'] )
+				)
 			);
 		}
 

public_html/wp-content/plugins/wordcamp-payments-network/includes/wordcamp-budgets-dashboard.php
@@ -609,6 +609,20 @@ function enqueue_scripts( $hook ) {
 }
 
 /**
+ * Converted a formatted amount into a float.
+ *
+ * e.g., `'1,234.00'` -> `1234`
+ *
+ * @return float
+ */
+function formatted_amount_to_float( string $amount ) {
+	$float_amount = wp_unslash( $amount );
+	$float_amount = str_replace( ',', '', $float_amount );
+
+	return (float) $float_amount;
+}
+
+/**
  * Format an amount for display
  *
  * @param float  $amount

commit 2734127899f534aa41a909f59915fe6d0b3fb231

Payments Network: Check search term type to allow names again.
Searching by names was broken by `2eb4f8347839021c9cae23e733751b84331529b7`.

public_html/wp-content/plugins/wordcamp-payments-network/includes/payment-requests-list-table.php
@@ -77,7 +77,12 @@ class Payment_Requests_List_Table extends WP_List_Table {
 		}
 
 		if ( ! empty( $_REQUEST['s'] ) ) {
-			$query = Budgets_Dashboard\formatted_amount_to_float( $_REQUEST['s'] );
+			// Support searching for both amounts and names.
+			if ( is_numeric( $_REQUEST['s'] ) ) {
+				$query = Budgets_Dashboard\formatted_amount_to_float( $_REQUEST['s'] );
+			} else {
+				$query = wp_unslash( $_REQUEST['s'] );
+			}
 
 			$where .= $wpdb->prepare(
 				' AND `keywords` LIKE %s ',

public_html/wp-content/plugins/wordcamp-payments-network/includes/reimbursement-requests-list-table.php
@@ -58,11 +58,16 @@ class Reimbursement_Requests_List_Table extends WP_List_Table {
 		$search     = '';
 
 		if ( ! empty( $_REQUEST['s'] ) ) {
+			// Support searching for both amounts and names.
+			if ( is_numeric( $_REQUEST['s'] ) ) {
+				$term = Budgets_Dashboard\formatted_amount_to_float( $_REQUEST['s'] );
+			} else {
+				$term = wp_unslash( $_REQUEST['s'] );
+			}
+
 			$search = $wpdb->prepare(
 				"AND `keywords` LIKE '%%%s%%'",
-				$wpdb->esc_like(
-					Budgets_Dashboard\formatted_amount_to_float( $_REQUEST['s'] )
-				)
+				$wpdb->esc_like( $term )
 			);
 		}
 