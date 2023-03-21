DomDev126/wordcamp.org

commit 723325ed2489e742ecc5f2953a02457d3540355e

Blocks: Check for post context values before using data (#851)
If used in the post editor, these are valid and reflect the current post. If used in the template editor, these are null, and the block should display a placeholder value. This will display the placeholder value in the template editor regardless of sessions (published or not) on the site.

Fixes #829, fixes #832

public_html/wp-content/mu-plugins/blocks/source/blocks/session-date/edit.js
@@ -11,16 +11,12 @@ import { __, _x } from '@wordpress/i18n';
 import { AlignmentControl, BlockControls, InspectorControls, useBlockProps } from '@wordpress/block-editor';
 import { dateI18n, __experimentalGetSettings as getDateSettings } from '@wordpress/date'; // eslint-disable-line @wordpress/no-unsafe-wp-apis
 import { PanelBody, SelectControl, ToggleControl } from '@wordpress/components';
-import { store as coreStore } from '@wordpress/core-data';
-import { useSelect } from '@wordpress/data';
+import { useEntityProp } from '@wordpress/core-data';
 
 export default function Edit( { attributes, setAttributes, context: { postId, postType } } ) {
 	const { format, showTimezone, textAlign } = attributes;
-	const date = useSelect( ( select ) => {
-		const { getEntityRecord } = select( coreStore );
-		const session = getEntityRecord( 'postType', postType, postId );
-		return session.meta._wcpt_session_time * 1000; // Convert from s to ms.
-	}, [] );
+	const [ meta = {} ] = useEntityProp( 'postType', postType, 'meta', postId );
+	const date = meta._wcpt_session_time;
 
 	const { formats } = getDateSettings();
 	const defaultFormat = formats.datetime;

public_html/wp-content/mu-plugins/blocks/source/blocks/meta-link/edit.js
@@ -15,19 +15,12 @@ import {
 	useBlockProps,
 } from '@wordpress/block-editor';
 import { Notice } from '@wordpress/components';
-import { store as coreStore } from '@wordpress/core-data';
-import { useSelect } from '@wordpress/data';
+import { useEntityProp } from '@wordpress/core-data';
 
 export default function Edit( { attributes, setAttributes, context: { postId, postType } } ) {
 	const { key, text, textAlign } = attributes;
-	const url = useSelect(
-		( select ) => {
-			const { getEntityRecord } = select( coreStore );
-			const post = getEntityRecord( 'postType', postType, postId );
-			return post.meta[ key ];
-		},
-		[ key ]
-	);
+	const [ meta = {} ] = useEntityProp( 'postType', postType, 'meta', postId );
+	const url = meta[ key ] || '';
 
 	const blockProps = useBlockProps( {
 		className: classnames( {
@@ -45,10 +38,13 @@ export default function Edit( { attributes, setAttributes, context: { postId, po
 					} }
 				/>
 			</BlockControls>
-			{ ! url && (
+			{ postId && postType && ! url && (
 				<InspectorControls>
 					<Notice status="error" isDismissible={ false }>
-						{ __( 'The link for this content is missing. Add the URL in the Session tab.', 'wordcamporg' ) }
+						{ __(
+							'The link for this content is missing. Add the URL in the Session tab.',
+							'wordcamporg'
+						) }
 					</Notice>
 				</InspectorControls>
 			) }

public_html/wp-content/mu-plugins/blocks/source/blocks/session-speakers/edit.js
@@ -15,16 +15,11 @@ import {
 	useBlockProps,
 } from '@wordpress/block-editor';
 import { PanelBody, ToggleControl } from '@wordpress/components';
-import { store as coreStore } from '@wordpress/core-data';
-import { useSelect } from '@wordpress/data';
+import { useEntityProp } from '@wordpress/core-data';
 
 export default function( { attributes, setAttributes, context: { postId, postType }, isSelected } ) {
 	const { byline, isLink, textAlign } = attributes;
-	const speakers = useSelect( ( select ) => {
-		const { getEntityRecord } = select( coreStore );
-		const session = getEntityRecord( 'postType', postType, postId );
-		return session.session_speakers || [];
-	}, [] );
+	const [ speakers = [] ] = useEntityProp( 'postType', postType, 'session_speakers', postId );
 
 	const blockProps = useBlockProps( {
 		className: classnames( {
@@ -62,11 +57,17 @@ export default function( { attributes, setAttributes, context: { postId, postTyp
 						onChange={ ( value ) => setAttributes( { byline: value } ) }
 					/>
 				) }
-				{ speakers.map( ( { id, name, link } ) => (
-					<span key={ id } className="wp-block-wordcamp-session-speakers__name">
-						{ isLink ? <a href={ link }>{ name }</a> : name }
+				{ postType && postId ? (
+					speakers.map( ( { id, name, link } ) => (
+						<span key={ id } className="wp-block-wordcamp-session-speakers__name">
+							{ isLink ? <a href={ link }>{ name }</a> : name }
+						</span>
+					) )
+				) : (
+					<span className="wp-block-wordcamp-session-speakers__name">
+						{ __( 'Speaker Name', 'wordcamporg' ) }
 					</span>
-				) ) }
+				) }
 			</div>
 		</>
 	);

public_html/wp-content/mu-plugins/blocks/source/blocks/speaker-sessions/edit.js
@@ -20,6 +20,18 @@ import { getSessionDetails, sortSessionByTime } from '../sessions/utils';
 export default function( { attributes, setAttributes, context: { postId } } ) {
 	const { hasSessionDetails, isLink, textAlign } = attributes;
 	const sessions = useSelect( ( select ) => {
+		if ( ! postId ) {
+			return [
+				{
+					id: 1,
+					title: { rendered: 'Session Name' },
+					link: '#',
+					session_date_time: { date: 'November 1, 2023', time: '10:15 am' },
+					session_track: [],
+				},
+			];
+		}
+
 		const { getEntityRecords } = select( coreStore );
 		const _sessions =
 			getEntityRecords( 'postType', 'wcb_session', {

commit 6997d265c3ae85846dc7582ba1508ace88b9753a

Blocks: Session Date: Convert date meta to ms
The `date` functions in JS expect values in `ms`, but the date meta is stored in seconds. This causes incorrect dates to render in the editor.

Fixes bug introduced in 723325ed2489e742ecc5f2953a02457d3540355e

public_html/wp-content/mu-plugins/blocks/source/blocks/session-date/edit.js
@@ -16,7 +16,7 @@ import { useEntityProp } from '@wordpress/core-data';
 export default function Edit( { attributes, setAttributes, context: { postId, postType } } ) {
 	const { format, showTimezone, textAlign } = attributes;
 	const [ meta = {} ] = useEntityProp( 'postType', postType, 'meta', postId );
-	const date = meta._wcpt_session_time;
+	const date = meta._wcpt_session_time * 1000; // Convert from s to ms.
 
 	const { formats } = getDateSettings();
 	const defaultFormat = formats.datetime;